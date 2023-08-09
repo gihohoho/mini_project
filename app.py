558b58d27a2c244071c8ad9620d243455437b887
JungGyu00 <co399955@gmail.com>
2023-08-09 14:47:58 +0900

    수정기능 추가

--- app.py
+++ app.py
@@ -16,74 +16,110 @@ def home():
 @app.route('/index_이기호.html')
 def ho_page():
     return render_template('index_이기호.html')
-# 개인서버로 저장
+# 저장 기능
 @app.route("/index_ho", methods=["POST"])
 def ho_post():
     detail_receive = request.form['detail_give']
     description_receive = request.form['description_give']
 
     doc = {
-        'name':detail_receive,
-        'comment':description_receive
+        'detail':detail_receive,
+        'description':description_receive
     } 
     db.team_ho.insert_one(doc)
 
     return jsonify({'msg': '저장완료!'})
+# 수정 기능
+@app.route("/index_ho_update", methods=["POST"])
+def ho_ud_post():
+    ud_detail_receive = request.form['ud_detail_give']
+    ud_description_receive = request.form['ud_description_give']
+
+    db.team_ho.update_one({'detail': ud_detail_receive},{'$set':{'description':ud_description_receive}})
+
+    return jsonify({'msg': '수정완료!'})
 
 # 솔님 주소
 @app.route('/index_이솔.html')
 def sol_page():
     return render_template('index_이솔.html')
-# 개인서버로 저장
+# 저장 기능
 @app.route("/index_sol", methods=["POST"])
 def sol_post():
     detail_receive = request.form['detail_give']
     description_receive = request.form['description_give']
 
     doc = {
-        'name':detail_receive,
-        'comment':description_receive
+        'detail':detail_receive,
+        'description':description_receive
     } 
     db.team_sol.insert_one(doc)
 
     return jsonify({'msg': '저장완료!'})
+# 수정 기능
+@app.route("/index_sol_update", methods=["POST"])
+def sol_ud_post():
+    ud_detail_receive = request.form['ud_detail_give']
+    ud_description_receive = request.form['ud_description_give']
+
+    db.team_sol.update_one({'detail': ud_detail_receive},{'$set':{'description':ud_description_receive}})
+
+    return jsonify({'msg': '수정완료!'})
 
 # 정규 주소
 @app.route('/index_김정규.html')
 def gyu_page():
     return render_template('index_김정규.html')
-# 개인서버로 저장
+# 저장 기능
 @app.route("/index_gyu", methods=["POST"])
 def gyu_post():
     detail_receive = request.form['detail_give']
     description_receive = request.form['description_give']
 
     doc = {
-        'name':detail_receive,
-        'comment':description_receive
+        'detail':detail_receive,
+        'description':description_receive
     } 
     db.team_gyu.insert_one(doc)
 
     return jsonify({'msg': '저장완료!'})
+# 수정 기능
+@app.route("/index_gyu_update", methods=["POST"])
+def gyu_ud_post():
+    ud_detail_receive = request.form['ud_detail_give']
+    ud_description_receive = request.form['ud_description_give']
+
+    db.team_gyu.update_one({'detail': ud_detail_receive},{'$set':{'description':ud_description_receive}})
+
+    return jsonify({'msg': '수정완료!'})
 
 
 # 제욱님 주소
 @app.route('/index_이제욱.html')
 def wook_page():
     return render_template('index_이제욱.html')
-# 개인서버로 저장
+# 저장 기능
 @app.route("/index_wook", methods=["POST"])
 def wook_post():
     detail_receive = request.form['detail_give']
     description_receive = request.form['description_give']
 
     doc = {
-        'name':detail_receive,
-        'comment':description_receive
+        'detail':detail_receive,
+        'description':description_receive
     } 
     db.team_wook.insert_one(doc)
 
     return jsonify({'msg': '저장완료!'})
+# 수정 기능
+@app.route("/index_wook_update", methods=["POST"])
+def wook_ud_post():
+    ud_detail_receive = request.form['ud_detail_give']
+    ud_description_receive = request.form['ud_description_give']
+
+    db.team_wook.update_one({'detail': ud_detail_receive},{'$set':{'description':ud_description_receive}})
+
+    return jsonify({'msg': '수정완료!'})
 
 
 if __name__ == '__main__':
