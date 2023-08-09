c87d49995eefc6b043dec90c8d62bb9b0b390c6d
solleepang <dlthf727@gmail.com>
2023-08-09 15:39:41 +0900

    추가후 내용 보이도록, 삭제 기능, 버튼 이동

--- app.py
+++ app.py
@@ -23,12 +23,29 @@ def ho_post():
     description_receive = request.form['description_give']
 
     doc = {
-        'name':detail_receive,
-        'comment':description_receive
-    } 
+        'detail': detail_receive,
+        'description': description_receive
+    }
     db.team_ho.insert_one(doc)
 
     return jsonify({'msg': '저장완료!'})
+
+
+# ho 서버에 저장한 정보 가져오기
+@app.route('/index_ho', methods=['GET'])
+def read_ho():
+    info = list(db.team_ho.find({}, {'_id': False}))
+    print(info)
+    return jsonify({'info_ho': info})
+
+
+# ho 서버에서 정보 삭제하기
+@app.route('/index_ho/delete', methods=['POST'])
+def delete_info_ho():
+    detail_receive = request.form['detail_give']
+    db.team_ho.delete_one({'detail': detail_receive})
+    return jsonify({'msg': '삭제 완료!'})
+
 
 # 솔님 주소
 @app.route('/index_이솔.html')
@@ -41,12 +58,26 @@ def sol_post():
     description_receive = request.form['description_give']
 
     doc = {
-        'name':detail_receive,
-        'comment':description_receive
-    } 
+        'detail': detail_receive,
+        'description': description_receive
+    }
     db.team_sol.insert_one(doc)
 
     return jsonify({'msg': '저장완료!'})
+
+# sol 서버에 저장한 정보 가져오기
+@app.route('/index_sol', methods=['GET'])
+def read_sol():
+    info = list(db.team_sol.find({}, {'_id': False}))
+    return jsonify({'info_sol': info})
+
+
+# sol 서버에서 정보 삭제하기
+@app.route('/index_sol/delete', methods=['POST'])
+def delete_info_sol():
+    detail_receive = request.form['detail_give']
+    db.team_sol.delete_one({'detail': detail_receive})
+    return jsonify({'msg': '삭제 완료!'})
 
 # 정규 주소
 @app.route('/index_김정규.html')
@@ -59,12 +90,26 @@ def gyu_post():
     description_receive = request.form['description_give']
 
     doc = {
-        'name':detail_receive,
-        'comment':description_receive
-    } 
+        'detail': detail_receive,
+        'description': description_receive
+    }
     db.team_gyu.insert_one(doc)
 
     return jsonify({'msg': '저장완료!'})
+
+# gyu 서버에 저장한 정보 가져오기
+@app.route('/index_gyu', methods=['GET'])
+def read_gyu():
+    info = list(db.team_gyu.find({}, {'_id': False}))
+    return jsonify({'info_gyu': info})
+
+
+# gyu 서버에서 정보 삭제하기
+@app.route('/index_gyu/delete', methods=['POST'])
+def delete_info_gyu():
+    detail_receive = request.form['detail_give']
+    db.team_gyu.delete_one({'detail': detail_receive})
+    return jsonify({'msg': '삭제 완료!'})
 
 
 # 제욱님 주소
@@ -78,16 +123,27 @@ def wook_post():
     description_receive = request.form['description_give']
 
     doc = {
-        'name':detail_receive,
-        'comment':description_receive
-    } 
+        'detail': detail_receive,
+        'description': description_receive
+    }
     db.team_wook.insert_one(doc)
 
     return jsonify({'msg': '저장완료!'})
 
+# 서버에 저장한 정보 가져오기
+@app.route('/index_wook', methods=['GET'])
+def read_wook():
+    info = list(db.team_wook.find({}, {'_id': False}))
+    return jsonify({'info_wook': info})
+
+
+# 서버에서 정보 삭제하기
+@app.route('/index_wook/delete', methods=['POST'])
+def delete_info_wook():
+    detail_receive = request.form['detail_give']
+    db.team_wook.delete_one({'detail': detail_receive})
+    return jsonify({'msg': '삭제 완료!'})
+
 
 if __name__ == '__main__':
-
     app.run('0.0.0.0', port=5000, debug=True)
-# install 안했음 코드수정 테스트
-
